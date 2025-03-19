a71888bca30984469482fee6767532006ece831d
JinFree <ytrqwe12@gmail.com>
2025-03-19 20:26:46 +0900

    train on colab

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -29,12 +29,14 @@ try:
 
     assert hasattr(wandb, '__version__')  # verify package import not local dir
     if pkg.parse_version(wandb.__version__) >= pkg.parse_version('0.12.2') and RANK in {0, -1}:
-        try:
-            wandb_login_success = wandb.login(timeout=30)
-        except wandb.errors.UsageError:  # known non-TTY terminal issue
-            wandb_login_success = False
-        if not wandb_login_success:
-            wandb = None
+        wandb_login_success = False
+        wandb = None
+        # try:
+        #     wandb_login_success = wandb.login(timeout=30)
+        # except wandb.errors.UsageError:  # known non-TTY terminal issue
+        #     wandb_login_success = False
+        # if not wandb_login_success:
+        #     wandb = None
 except (ImportError, AssertionError):
     wandb = None
 
